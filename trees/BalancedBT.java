2c32f4555e1525d2a9e4e022e283e927f7b433f4
Ankit Sharma <beingbmc12@gmail.com>
2019-06-01 12:44:47 +0530

    Create BalancedBT.java

---
+++ trees/BalancedBT.java
@@ -0,0 +1,11 @@
+class Solution {
+    private int checkBalance(TreeNode root){
+        if(root == null) return 0;
+        int left = checkBalance(root.left);
+        int right = checkBalance(root.right);
+        return (left == -1 || right == -1 || Math.abs(left - right) > 1) ? -1 : Math.max(left, right) + 1;
+    }
+    public boolean isBalanced(TreeNode root) {
+        return checkBalance(root) != -1;
+    }
+}
